56cc69f597a58ba053ee7d34b062afb6e29071fa
georgianamargi <maris.marginean@gmail.com>
2026-07-31 14:21:11 +0300

    Update Program.cs

--- backend/Program.cs
+++ backend/Program.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddSignalR();
 
 var connectionString =
     builder.Configuration.GetConnectionString("DefaultConnection")
